6bd052a78b47dd2e1354ee04a528da9b964fda02
Michael Haddad <michael@ludditetechnology.com>
2011-05-19 11:43:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# poundie-ffm
+
+A Poundie plugin for discovering the antipodal restaurant for the provided location.
+
+## USAGE
+
+Just `require "poundie-ffm"` in your Poundie bot, then post something like:
+
+    poundie ffm detroit
---
+++ lib/poundie-ffm.rb
@@ -0,0 +1,11 @@
+class PoundieFfm < Poundie::Plugin
+  register :poundie_ffm
+
+  prefix %r/poundie ffm\s+/ do |term|
+    response = JSON.parse(`curl http://farthestfromme.com/api/antipodal_property.js?q=#{CGI.escape(term)}`)
+    property = response["response"]["antipodal_property"]
+    speak "The antipodal restaurant to #{term} is #{property["name"]} @ #{property["lat"]}, #{property["lng"]}"
+  end
+end
+
+Poundie.use :poundie_ffm
