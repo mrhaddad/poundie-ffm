7117968db37ebb793298bfe8a5c0d06094f88b87
Michael Haddad <michael@ludditetechnology.com>
2011-05-19 14:32:55 -0400

    add map

--- lib/poundie-ffm.rb
+++ lib/poundie-ffm.rb
@@ -5,8 +5,10 @@ class PoundieFfm < Poundie::Plugin
 
   prefix %r/poundie ffm\s+/ do |term|
     response = JSON.parse(`curl http://farthestfromme.com/api/antipodal_property.js?q=#{CGI.escape(term)}`)
+    location = response["response"]["location"]
     property = response["response"]["antipodal_property"]
-    speak "The antipodal restaurant to #{term} is #{property["name"]} @ #{property["lat"]}, #{property["lng"]}"
+    speak "The antipodal restaurant to #{location["name"]} is #{property["name"]} @ #{property["lat"]}, #{property["lng"]}."
+    speak "http://maps.google.com/maps/api/staticmap?size=512x512&maptype=roadmap&markers=color:green|label:A|#{location["lat"]},#{location["lng"]}&markers=color:red|label:B|#{property["lat"]},#{property["lng"]}&path=color:0x0000ff|weight:5|#{location["lat"]},#{location["lng"]}|#{property["lat"]},#{property["lng"]}&sensor=false"
   end
 end
 
